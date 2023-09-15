3ed1e1b24aa560fe611120c5badb521fe3cc4f86
wb_yanglipeng02 <wb_yanglipeng02@meituan.com>
2023-09-15 17:13:51 +0800

    wec ommit

---
+++ learn-langchain/src/app.ts
@@ -0,0 +1,78 @@
+import express from "express";
+import http from "http";
+import createError from "http-errors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import main from "./main";
+import "reflect-metadata";
+
+require("dotenv").config();
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+
+main(app, server);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/", (req, res) => {});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err: any, req: any, res: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: any) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr!.port;
+  console.log("Listening on " + bind);
+}
---
+++ learn-langchain/src/main.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { Server } from "http";
+import { Express } from "express";
+import { DataSource } from "typeorm";
+import { Photo } from "./Entity/Photo";
+import { SqlDatabase } from "langchain/sql_db";
+import { SqlDatabaseChain } from "langchain/chains/sql_db";
+import { OpenAI } from "langchain/llms/openai";
+
+const dbFile = path.resolve("src/Chinook.db");
+export default async function (app: Express, server: Server) {
+  const datasource = new DataSource({
+    type: "sqlite",
+    database: dbFile,
+    entities: [Photo],
+    synchronize: true,
+  });
+  await datasource.initialize();
+
+  // for (let id = 0; id < 100; id++) {
+  //   const photo = new Photo();
+  //   photo.description = "我的肖像画";
+  //   photo.filename = "a.png";
+  //   photo.name = "ylp" + id;
+  //   photo.views = 100;
+  //   photo.isPublished = false;
+  //   datasource.manager.save(photo);
+  // }
+
+  // const firstUser = await datasource
+  //   .getRepository(Photo)
+  //   .createQueryBuilder("photo")
+  //   .getCount();
+
+  // console.log(firstUser);
+
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
+
+    const chain = new SqlDatabaseChain({
+      llm: new OpenAI({ temperature: 0 }),
+      database: db,
+    });
+
+    const res = await chain.run("How many tracks are there?");
+    console.log(res);
+}
